ca6d64835a880b16a11c75f3d02bc71e53d59a67
Nick Burlingame <nicholasaburlingame@gmail.com>
2018-04-08 19:19:48 -0400

    Added UI and the ability to roll dice. Currently only works with D4

---
+++ Assets/Scripts/Managers/DiceManager.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiceManager : MonoBehaviour {
+
+    [HideInInspector]
+    public GameObject[] m_values;
+    [HideInInspector]
+    public Texture m_texture;
+    [HideInInspector]
+    public GameObject m_Instance;
+    public bool isMoving;
+    public Vector3 lastPosition;
+    public Vector3 secondLastPosition;
+
+
+
+
+
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    public void RandomizeDice()
+    {
+        var a = 1;
+        m_Instance.transform.Rotate(UnityEngine.Random.Range(0.0f, 360.0f), UnityEngine.Random.Range(0.0f, 360.0f), UnityEngine.Random.Range(0.0f, 360.0f));
+    }
+
+    public int GetValue()
+    {
+        int selectedValue = 0;
+        GameObject values = m_Instance.transform.Find("Values").gameObject;
+        GameObject currentValue;
+        float highestValue = 0;
+        for (int i = 0; i < values.transform.childCount; i++)
+        {
+            currentValue = values.transform.GetChild(i).gameObject;
+            if (currentValue.transform.position.y > highestValue)
+            {
+                highestValue = currentValue.transform.position.y;
+                selectedValue = Convert.ToInt32(currentValue.name);
+            }
+        }
+       
+        return selectedValue;
+    }
+
+    public bool GetIsMoving()
+    {
+        bool isMoving = true;
+        Vector3 currentPosition = m_Instance.transform.position;
+        if (currentPosition.Equals(lastPosition) && currentPosition.Equals(secondLastPosition))
+        {
+            isMoving = false;
+        }
+        else
+        {
+            isMoving = true;
+        }
+        secondLastPosition = lastPosition;
+        lastPosition = currentPosition;
+        
+        return isMoving;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+}
--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,205 +1,268 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using System;
 
-public class GameManager : MonoBehaviour
-{
-    public int m_NumRoundsToWin = 5;        
-    public float m_StartDelay = 3f;         
-    public float m_EndDelay = 3f;           
-    public CameraControl m_CameraControl;   
-    public Text m_MessageText;              
-    public GameObject m_TankPrefab;         
-    public TankManager[] m_Tanks;           
+public class GameManager : MonoBehaviour {
 
+    public float m_StartDelay = .01f;
+    public CameraControl m_CameraControl;
+    public Canvas m_MessageCanvas;
+    public Button btnRoll;
+    public GameObject DiceSelector;
+    public GameObject D2;
+    public GameObject D4;
+    public GameObject D6;
+    public GameObject D10;
+    public GameObject D12;
+    public GameObject D20;
+    public GameObject D100;
+    public int diceSelectionAdjustment;
 
-    private int m_RoundNumber;              
-    private WaitForSeconds m_StartWait;     
-    private WaitForSeconds m_EndWait;       
-    private TankManager m_RoundWinner;
-    private TankManager m_GameWinner;       
+    private WaitForSeconds m_StartWait;
+    private List<DiceSet> m_DiceSets;
+    private bool doneRolling = false;
+    private int diceSelectorsTotal = 1;
 
-
-    private void Start()
+    public struct DiceSet
     {
+        public List<DiceManager> m_Dice;
+        public int modifier;
+        public int total;
+    }
+
+    // Use this for initialization
+    void Start () {
         m_StartWait = new WaitForSeconds(m_StartDelay);
-        m_EndWait = new WaitForSeconds(m_EndDelay);
-
-        SpawnAllTanks();
-        SetCameraTargets();
-
         StartCoroutine(GameLoop());
     }
 
-
-    private void SpawnAllTanks()
-    {
-        for (int i = 0; i < m_Tanks.Length; i++)
-        {
-            m_Tanks[i].m_Instance =
-                Instantiate(m_TankPrefab, m_Tanks[i].m_SpawnPoint.position, m_Tanks[i].m_SpawnPoint.rotation) as GameObject;
-            m_Tanks[i].m_PlayerNumber = i + 1;
-            m_Tanks[i].Setup();
-        }
-    }
-
-
-    private void SetCameraTargets()
-    {
-        Transform[] targets = new Transform[m_Tanks.Length];
-
-        for (int i = 0; i < targets.Length; i++)
-        {
-            targets[i] = m_Tanks[i].m_Instance.transform;
-        }
-
-        m_CameraControl.m_Targets = targets;
-    }
-
-
     private IEnumerator GameLoop()
     {
-        yield return StartCoroutine(RoundStarting());
-        yield return StartCoroutine(RoundPlaying());
-        yield return StartCoroutine(RoundEnding());
+        //yield return StartCoroutine(ShowIntroScreen());
+        yield return StartCoroutine(ShowSelectScreen());
+        //DiceManager currentDice = new DiceManager();
+        //DiceSet currentDiceSet = new DiceSet();
+        m_DiceSets = new List<DiceSet>();
+        //currentDiceSet.m_Dice = new List<DiceManager>();
 
-        if (m_GameWinner != null)
-        {
-            SceneManager.LoadScene(0);
-        }
-        else
-        {
-            StartCoroutine(GameLoop());
-        }
+        //currentDice = SetDiceType(currentDice, 4);
+        //currentDice.RandomizeDice();
+        //currentDice.m_Instance.transform.position = new Vector3(0, 10, 0);
+        //currentDiceSet.m_Dice.Add(currentDice);
+        //currentDiceSet.modifier = 2;
+        //m_DiceSets.Add(currentDiceSet);
     }
 
-
-    private IEnumerator RoundStarting()
+    private IEnumerator ShowIntroScreen()
     {
-        ResetAllTanks();
-        DisableTankControl();
-        m_CameraControl.SetStartPositionAndSize();
-        m_RoundNumber++;
-        m_MessageText.text = "ROUND " + m_RoundNumber;
+        GameObject introScreen = m_MessageCanvas.transform.Find("IntroScreen").gameObject;
+        GameObject selectionScreen = m_MessageCanvas.transform.Find("SelectionScreen").gameObject;
+        introScreen.SetActive(true);
+        selectionScreen.SetActive(false);
+        yield return m_StartWait;
+    }
+
+    private IEnumerator ShowSelectScreen()
+    {
+        GameObject introScreen = m_MessageCanvas.transform.Find("IntroScreen").gameObject;
+        GameObject selectionScreen = m_MessageCanvas.transform.Find("SelectionScreen").gameObject;
+        GameObject diceSelectors = selectionScreen.transform.Find("DiceSelectors").gameObject;
+        introScreen.SetActive(false);
+        selectionScreen.SetActive(true);
+        GameObject diceSelector = diceSelectors.transform.Find("DiceSelector").gameObject;
+        GameObject btnAddNew = diceSelector.transform.Find("btnAddNew").gameObject;
+        GameObject btnRemove = diceSelector.transform.Find("btnRemove").gameObject;
+        btnAddNew.GetComponent<Button>().onClick.AddListener(delegate { AddNewClick(); });
+        btnRemove.GetComponent<Button>().onClick.AddListener(delegate { RemoveClick(0); });
+        btnRoll.GetComponent<Button>().onClick.AddListener(delegate { RollClick(); });
+        diceSelector.name = "DiceSelector1";
         yield return m_StartWait;
     }
 
 
-    private IEnumerator RoundPlaying()
+    void AddNewClick()
     {
-        EnableTankControl();
-        m_MessageText.text = string.Empty;
-        while (!OneTankLeft())
-        {
-            yield return null;
+        GameObject selectionScreen = m_MessageCanvas.transform.Find("SelectionScreen").gameObject;
+        GameObject diceSelectors = selectionScreen.transform.Find("DiceSelectors").gameObject;
+        GameObject lastSelector = FindLastSelector(diceSelectors);
+        GameObject diceSelector;
+        GameObject btnAddNew;
+        GameObject btnRemove;
+
+        if (diceSelectors.transform.childCount < 6) {
+            diceSelector = GameObject.Instantiate(DiceSelector);
+            diceSelector.transform.parent = diceSelectors.transform;
+            btnRoll.transform.position = new Vector3(btnRoll.transform.position.x, btnRoll.transform.position.y - diceSelectionAdjustment, btnRoll.transform.position.z);
+            diceSelector.transform.position = new Vector3(lastSelector.transform.position.x, lastSelector.transform.position.y - diceSelectionAdjustment, lastSelector.transform.position.z);
+            btnAddNew = diceSelector.transform.Find("btnAddNew").gameObject;
+            btnRemove = diceSelector.transform.Find("btnRemove").gameObject;
+            btnAddNew.GetComponent<Button>().onClick.AddListener(delegate { AddNewClick(); });
+            btnRemove.GetComponent<Button>().onClick.AddListener(delegate { RemoveClick(diceSelectors.transform.childCount - 1); });
+            diceSelector.name = "DiceSelector" + diceSelectors.transform.childCount;
         }
-        
     }
-
-
-    private IEnumerator RoundEnding()
+    
+    void RemoveClick(int id)
     {
-        DisableTankControl();
+        GameObject selectionScreen = m_MessageCanvas.transform.Find("SelectionScreen").gameObject;
+        GameObject diceSelectors = selectionScreen.transform.Find("DiceSelectors").gameObject;
+        GameObject selectorToRemove = diceSelectors.transform.GetChild(id).gameObject;
+        GameObject currentSelector;
+        if (diceSelectors.transform.childCount > 1) { 
+            Destroy(selectorToRemove);
+            for(int i = id; i < diceSelectors.transform.childCount; i++)
+            {
+                currentSelector = diceSelectors.transform.GetChild(i).gameObject;
+                currentSelector.transform.position = new Vector3(currentSelector.transform.position.x, currentSelector.transform.position.y + diceSelectionAdjustment, currentSelector.transform.position.z);
+                currentSelector.name = "DiceSelector" + i;
+            }
 
-        m_RoundWinner = null;
-
-        m_RoundWinner = GetRoundWinner();
-
-        if(m_RoundWinner != null)
-            m_RoundWinner.m_Wins++;
-
-        m_GameWinner = GetGameWinner();
-
-        string message = EndMessage();
-        m_MessageText.text = message;
-
-        yield return m_EndWait;
-    }
-
-
-    private bool OneTankLeft()
-    {
-        int numTanksLeft = 0;
-
-        for (int i = 0; i < m_Tanks.Length; i++)
-        {
-            if (m_Tanks[i].m_Instance.activeSelf)
-                numTanksLeft++;
-        }
-
-        return numTanksLeft <= 1;
-    }
-
-
-    private TankManager GetRoundWinner()
-    {
-        for (int i = 0; i < m_Tanks.Length; i++)
-        {
-            if (m_Tanks[i].m_Instance.activeSelf)
-                return m_Tanks[i];
-        }
-
-        return null;
-    }
-
-
-    private TankManager GetGameWinner()
-    {
-        for (int i = 0; i < m_Tanks.Length; i++)
-        {
-            if (m_Tanks[i].m_Wins == m_NumRoundsToWin)
-                return m_Tanks[i];
-        }
-
-        return null;
-    }
-
-
-    private string EndMessage()
-    {
-        string message = "DRAW!";
-
-        if (m_RoundWinner != null)
-            message = m_RoundWinner.m_ColoredPlayerText + " WINS THE ROUND!";
-
-        message += "\n\n\n\n";
-
-        for (int i = 0; i < m_Tanks.Length; i++)
-        {
-            message += m_Tanks[i].m_ColoredPlayerText + ": " + m_Tanks[i].m_Wins + " WINS\n";
-        }
-
-        if (m_GameWinner != null)
-            message = m_GameWinner.m_ColoredPlayerText + " WINS THE GAME!";
-
-        return message;
-    }
-
-
-    private void ResetAllTanks()
-    {
-        for (int i = 0; i < m_Tanks.Length; i++)
-        {
-            m_Tanks[i].Reset();
+            btnRoll.transform.position = new Vector3(btnRoll.transform.position.x, btnRoll.transform.position.y + diceSelectionAdjustment, btnRoll.transform.position.z);
         }
     }
 
-
-    private void EnableTankControl()
+    void RollClick()
     {
-        for (int i = 0; i < m_Tanks.Length; i++)
+        GameObject selectionScreen = m_MessageCanvas.transform.Find("SelectionScreen").gameObject;
+        GameObject diceSelectors = selectionScreen.transform.Find("DiceSelectors").gameObject;
+        int currentDiceCount = 0;
+        int currentModifier = 0;
+        int currentDiceType = 0;
+        int selectedDiceTypeValue = 0;
+
+        for (int i = 0; i < diceSelectors.transform.childCount; i++)
         {
-            m_Tanks[i].EnableControl();
+            GameObject diceSelector = diceSelectors.transform.GetChild(i).gameObject;
+            DiceSet currentDiceSet = new DiceSet();
+
+            GameObject txtDiceCount = diceSelector.transform.Find("txtDiceCount").gameObject;
+            GameObject txtModifier = diceSelector.transform.Find("txtModifier").gameObject;
+            GameObject ddlDiceSelector = diceSelector.transform.Find("ddlDiceSelector").gameObject;
+
+            
+
+            Text maybe = txtDiceCount.transform.Find("Text").GetComponent<Text>();
+
+
+            
+
+            currentDiceCount = Convert.ToInt32(txtDiceCount.transform.Find("Text").GetComponent<Text>().text);
+            currentModifier = Convert.ToInt32(txtModifier.transform.Find("Text").GetComponent<Text>().text);
+            selectedDiceTypeValue = Convert.ToInt32(ddlDiceSelector.GetComponent<Dropdown>().value);
+            currentDiceType = Convert.ToInt32(ddlDiceSelector.GetComponent<Dropdown>().options[selectedDiceTypeValue].text);
+
+            currentDiceSet.m_Dice = new List<DiceManager>();
+            currentDiceSet.modifier = currentModifier;
+
+            for(int j = 0; j < currentDiceCount; j++)
+            {
+                DiceManager currentDice = new DiceManager();
+                currentDice = SetDiceType(currentDice, currentDiceType);
+                currentDice.RandomizeDice();
+                int xCoordinate = 0;
+                int zCoordinate = 0;
+                if (j > currentDiceCount / 2)
+                {
+                    xCoordinate = -1 * (j + 1);
+                }
+                else
+                {
+                    xCoordinate = (j + 1);
+                }
+
+                if (i > diceSelectors.transform.childCount / 2)
+                {
+                    zCoordinate = -1 * (i + 1);
+                }
+                else
+                {
+                    zCoordinate = (i + 1);
+                }
+                
+
+                currentDice.m_Instance.transform.position = new Vector3(xCoordinate, 10, zCoordinate);
+                currentDiceSet.m_Dice.Add(currentDice);
+            }
+            m_DiceSets.Add(currentDiceSet);
         }
+        selectionScreen.SetActive(false);
+    }
+
+    GameObject FindLastSelector(GameObject diceSelectors)
+    {
+        GameObject lastSelector;
+        lastSelector = diceSelectors.transform.GetChild(diceSelectors.transform.childCount - 1).gameObject;
+
+        return lastSelector;
     }
 
 
-    private void DisableTankControl()
+    public DiceManager SetDiceType(DiceManager dice, int diceType)
     {
-        for (int i = 0; i < m_Tanks.Length; i++)
+        switch (diceType)
         {
-            m_Tanks[i].DisableControl();
+            case 2:
+                dice.m_Instance = GameObject.Instantiate(D2);
+                break;
+            case 4:
+                dice.m_Instance = GameObject.Instantiate(D4);
+                break;
+        }
+        return dice;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if(m_DiceSets != null) { 
+            doneRolling = IsDoneRolling();
+      
+            if (doneRolling == true)
+            {
+                GetTotals();
+            }
+        }
+    }
+
+    bool IsDoneRolling()
+    {
+        bool noMovement = false;
+        for (int i = 0; i < m_DiceSets.Count; i++)
+        {
+            DiceSet currentDiceSet = m_DiceSets[i];
+            for (int j = 0; j < currentDiceSet.m_Dice.Count; j++)
+            {
+                DiceManager currentDice = currentDiceSet.m_Dice[j];
+                if (currentDice.GetIsMoving() == false)
+                {
+                    noMovement = true;
+                    break;
+                }
+            }
+            if (noMovement == true)
+            {
+                break;
+            }
+        }
+        return noMovement;
+    }
+
+    void GetTotals()
+    {
+        int diceSetTotal = 0;
+        for (int i = 0; i < m_DiceSets.Count; i++)
+        {
+            diceSetTotal = 0;
+            DiceSet currentDiceSet = m_DiceSets[i];
+            for (int j = 0; j < currentDiceSet.m_Dice.Count; j++)
+            {
+                DiceManager currentDice = currentDiceSet.m_Dice[j];
+                diceSetTotal += currentDice.GetValue();
+            }
+            diceSetTotal += currentDiceSet.modifier;
+            currentDiceSet.total = diceSetTotal;
         }
     }
 }
